5381ac70b8f9f946bfea853a257772b02e30dc71
spallya <spallya@outlook.com>
2017-09-20 22:01:54 +0530

    Adding the implementation for adapter and bridge for structural design patterns

---
+++ src/com/patterns/structural/bridge/shape1/Circle.java
@@ -0,0 +1,5 @@
+package com.patterns.structural.bridge.shape1;
+
+public abstract class Circle extends Shape {
+
+}
---
+++ src/com/patterns/structural/bridge/shape2/Color.java
@@ -0,0 +1,7 @@
+package com.patterns.structural.bridge.shape2;
+
+public interface Color {
+
+	public void applyColor();
+	
+}
