b248c7acde4fc708d5254b9680f3bcd7df7b2153
spallya <spallya@outlook.com>
2017-09-26 23:12:43 +0530

    Adding implementation of decorator pattern

---
+++ src/com/patterns/structural/decorator/Sandwich.java
@@ -0,0 +1,7 @@
+package com.patterns.structural.decorator;
+
+public interface Sandwich {
+	
+	public String make();
+
+}
---
+++ src/com/patterns/structural/decorator/SimpleSandwich.java
@@ -0,0 +1,10 @@
+package com.patterns.structural.decorator;
+
+public class SimpleSandwich implements Sandwich {
+
+	@Override
+	public String make() {
+		return "Bread";
+	}
+
+}
